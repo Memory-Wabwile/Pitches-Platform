45b1df87d193fa045b59bcfcc2577e61859f7135
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-05 23:09:37 +0300

    create the navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import DevConfig
+
+
 
 #initializing application
-app = Flask(__name__)
+app = Flask(__name__ , instance_relative_config = True)
+
+#Setting up configurations
+app.config.from_object(DevConfig)
+# app.config.from_pyfile("config.py")
+
+#initialzing flask extensions
+bootstrap = Bootstrap(app)
+
+
 from . import views

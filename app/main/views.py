8ce2635a23053fe06ef5374bd194e22453b7be3f
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-06 20:24:08 +0300

    add the pitches heading on the home page

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 from . import main
 from flask_login import login_required
 
@@ -8,3 +9,12 @@ def index():
     function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)

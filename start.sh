ce8743e1ad3bddffead8a70d73f8cccafcf65ed1
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-07 18:18:05 +0300

    create the emails.py file to handle the email logic

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='wabwile'
 export SQLALCHEMY_DATABASE_URI='/postgresql+psycopg2://memory:MEM99ory@localhost/pitches'
-export MAIL_USERNAME=""
-export MAIL_PASSWORD=""
+export MAIL_USERNAME="nimotriz@gmail.com"
+export MAIL_PASSWORD="wabwileh"
 
 python3 manage.py server

04494d2fc30ca051be5aeab6c865214b651b9a33
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-09 15:40:44 +0300

    add welcme message to email

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 # from turtle import title
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm 
@@ -9,21 +9,27 @@ from .. import db
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('main.index'))
     login_form = LoginForm()
+
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
-        if user is None or not user.verify_password(login_form.password.data):
+        # user1 = User.query.filter_by(password = login_form.password.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,remember = login_form.remember.data)
             
-            flash('Invalid username or Password')
-            return redirect(url_for('login'))
-
-        login_user(user,remember = login_form.remember.data)
-        return redirect(url_for('index'))  
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid email or Password')
 
         
+        return redirect(url_for('auth.login')) 
 
-        # title = "pitches login"
-    return render_template('auth/login.html', login_form = login_form )
+        
+        # return redirect(url_for('main.index'))    
+
+    title = "pitches login"
+    return render_template('auth/login.html', login_form = login_form,title=title )
 
 @auth.route('/logout')
 @login_required
